9608d912ad0befb3137f4ddeef800b4692565960
Flia. Valdez-Vargas <Flia. Valdez-Vargas@DESKTOP-0BBIMTT>
2021-03-25 15:31:50 -0300

    Inicialización del proyecto

---
+++ SegundoProyectoSpringEDM/src/main/java/ar/unju/edu/edm/SegundoProyectoSpringEdmApplication.java
@@ -0,0 +1,13 @@
+package ar.unju.edu.edm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SegundoProyectoSpringEdmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SegundoProyectoSpringEdmApplication.class, args);
+	}
+
+}
---
+++ SegundoProyectoSpringEDM/src/test/java/ar/unju/edu/edm/SegundoProyectoSpringEdmApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.unju.edu.edm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SegundoProyectoSpringEdmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
